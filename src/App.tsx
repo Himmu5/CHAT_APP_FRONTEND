d71d38b078cf3dd89a97d57608ff95e99236c247
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-07-16 10:33:32 +0530

    base_URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { FC } from "react";
 import LogInAndRegister from "./Component/Register/LogInAndRegister";
 import UserProvider from "./Component/Providers/UserProvider";
-// import axios from "axios";
+import axios from "axios";
 import { Route, Routes } from "react-router-dom";
 import Chat from "./Component/pages/Chat";
 
-type P = {};
+type P = object
 
 const App: FC<P> = () => {
-  // axios.defaults.baseURL = `http://localhost:1000`;
-  // axios.defaults.withCredentials = true;
+  axios.defaults.baseURL = `https://himmu-chat-backend.onrender.com/`;
 
   return (
     <div>
