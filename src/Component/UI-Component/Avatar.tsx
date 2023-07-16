98089ac9e23c37b35303a6a44800d8087e262ad9
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-07-16 11:51:32 +0530

    Root url

--- src/Component/UI-Component/Avatar.tsx
+++ src/Component/UI-Component/Avatar.tsx
@@ -8,7 +8,7 @@ type P = {
 const Avatar: FC<P> = ({ username }) => {
   return (
     <div className="h-8 w-8 rounded-full bg-blue-500 text-white flex items-center ">
-      <div className="w-full text-center">{username.charAt(0) || "H"}</div>
+      <div className="w-full text-center">{username?.charAt(0) || "H"}</div>
     </div>
   );
 };
